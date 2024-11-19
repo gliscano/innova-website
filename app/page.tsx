43b4f545484e5745da9581562605cde908bbc83b
Guillermo Rojas <ing.guillermorojas@gmail.com>
2024-11-19 05:56:50 -0300

    fix link to store temporal

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://store.innova54.com/"
+            href="https://innova54store.empretienda.com.ar/"
             target="_blank"
             rel="noopener noreferrer"
           >
