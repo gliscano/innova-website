9e8daed516cf4702c060f53faeb0c7294d7b97f1
Guillermo Rojas <ing.guillermorojas@gmail.com>
2025-01-23 01:19:34 -0300

    update link to price list

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://drive.google.com/file/d/1DuDfFYbicBrnVh1gldjAU1ig-ZGC7iHa/view"
+            href="https://drive.google.com/file/d/1pEjFLUMFoQP1C3cwVas3-uwHNa3LNuwd/view"
             target="_blank"
             rel="noopener noreferrer"
           >
